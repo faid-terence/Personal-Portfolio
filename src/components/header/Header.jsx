c1916fa4fd1e50c7d6e86ce023e3944b2f005cba
Faid Terence JABO <faidterence@outlook.com>
2023-08-14 19:43:45 +0200

    feature(Header):Responsiveness

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import "./header.css";
+const Header = () => {
+    const [Toggle, showMenu] = useState(false);
+      
+  return (
+    <header className="header">
+      <nav className="nav container">
+        <a href="" className="nav__logo">
+          Terence FJ
+        </a>
+        <div className={Toggle? "nav__menu show-menu" : "nav__menu"}>
+          <ul className="nav__list grid">
+            <li className="nav__item">
+              <a href="" className="nav__link">
+                <i className="uil uil-estate nav__icon active-link"></i> Home
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="" className="nav__link">
+                <i className="uil uil-user nav__icon"></i> About
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="" className="nav__link">
+                <i className="uil uil-user nav__icon"></i> Skills
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="" className="nav__link">
+                <i className="uil uil-briefcase-alt nav__icon"></i> Services
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="" className="nav__link">
+                <i className="uil uil-scenery nav__icon"></i> Portfolio
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="" className="nav__link">
+                <i className="uil uil-message nav__icon"></i> Contact Me
+              </a>
+            </li>
+          </ul>
+          <i className="uil uil-times nav__close" onClick={() =>showMenu(!Toggle)}></i>
+        </div>
+
+        <div className="nav__toggle" onClick={()=> showMenu(!Toggle)}>
+          <i className="uil uil-apps"></i>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
