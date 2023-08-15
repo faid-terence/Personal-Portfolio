fd94d61bc54a0e3546bb252f85eb1918143a5de4
Faid Terence JABO <faidterence@outlook.com>
2023-08-15 17:48:53 +0200

    feature(ServiceSection):Implementation & responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import About from './components/About/About'
 import Home from './components/Home/Home'
+import Services from './components/Services/Services'
 import Skills from './components/Skills/Skills'
 import Header from './components/header/Header'
 
@@ -14,6 +15,7 @@ function App() {
         <Home/>
         <About/>
         <Skills/>
+        <Services/>
       </main>
     </>
   )
