c46ca0239117ee28e91503f77e73087acfb250df
Faid Terence JABO <faidterence@outlook.com>
2023-08-15 22:16:07 +0200

    feature(Qualifications):Section Implemented

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import About from './components/About/About'
 import Home from './components/Home/Home'
+import Qualifications from './components/Qualifications/Qualifications'
 import Services from './components/Services/Services'
 import Skills from './components/Skills/Skills'
 import Header from './components/header/Header'
@@ -16,6 +17,7 @@ function App() {
         <About/>
         <Skills/>
         <Services/>
+        <Qualifications/>
       </main>
     </>
   )
---
+++ src/components/Qualifications/Qualifications.jsx
@@ -0,0 +1,183 @@
+import "./qualification.css";
+import { useState } from "react";
+
+const Qualifications = () => {
+  const [toggleState, setToggleState] = useState(1);
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
+  return (
+    <section className="qualificcation section">
+      <h2 className="section__title">Qualifications</h2>
+      <span className="section__subtitle">My Personal Journey</span>
+
+      <div className="qualification__container container">
+        <div className="qualification__tabs">
+          <div
+            className={
+              toggleState === 1
+                ? "qualification__button qualification__active button--flex"
+                : "qualification__button qualification__active button--flex"
+            } onClick={()=> toggleTab(1)}
+          >
+            <i className="uil uil-graduation-cap qualification__icon"></i>
+            Education
+          </div>
+
+          <div className={
+              toggleState === 2
+                ? "qualification__button qualification__active button--flex"
+                : "qualification__button qualification__active button--flex"
+            } onClick={()=> toggleTab(2)}>
+            <i className="uil uil-briefcase-alt qualification__icon"></i>
+            Experience
+          </div>
+        </div>
+
+        <div className="qualification__secctions">
+          <div className="qualification_content qualification__content-active">
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Computer Engineering</h3>
+                <span className="qualification__subtitle">
+                  University of Rwanda
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2022 - Present
+                </div>
+              </div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div></div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+              <div>
+                <h3 className="qualification__title">Mobile App Developer</h3>
+                <span className="qualification__subtitle">
+                  Techly Africa
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2023 - Present
+                </div>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Math-Physics and Computer Science</h3>
+                <span className="qualification__subtitle">
+                  Lycee de Kigali
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2018 - 2021
+                </div>
+              </div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div></div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+              <div>
+                <h3 className="qualification__title">Full Stack Developer</h3>
+                <span className="qualification__subtitle">
+                  Andela Rwanda
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2022 - Present
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="qualification_content">
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Mobile App Developer</h3>
+                <span className="qualification__subtitle">
+                  Techly Africa
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2023 - Present
+                </div>
+              </div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div></div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+              <div>
+                <h3 className="qualification__title">Full Stack Developer</h3>
+                <span className="qualification__subtitle">
+                  Andela Rwanda
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2023 - Present
+                </div>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Web Designer</h3>
+                <span className="qualification__subtitle">
+                  University of Rwanda
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2022 - Present
+                </div>
+              </div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div></div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+              <div>
+                <h3 className="qualification__title">UI/UX Designer</h3>
+                <span className="qualification__subtitle">
+                  Personal
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2022 - Present
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Qualifications;
