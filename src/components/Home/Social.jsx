a53c83996d3a409b3a613409590c1bdb8eea00e3
Faid Terence JABO <faidterence@outlook.com>
2023-08-30 22:31:40 +0200

    bug(Home): Fix Links

--- src/components/Home/Social.jsx
+++ src/components/Home/Social.jsx
@@ -1,15 +1,16 @@
 const Social = () => {
   return (
     <div className="home__social">
-      <a href="" className="home__social-icon" target="_blank">
+      <a href="https://www.instagram.com/thestunna_420/" className="home__social-icon" target="_blank" rel="noreferrer">
         <i className="uil uil-instagram"></i>
       </a>
-      <a href="" className="home__social-icon" target="_blank">
+      <a href="https://twitter.com/thestunna_420" className="home__social-icon" target="_blank" rel="noreferrer">
         <i className="uil uil-twitter"></i>
       </a>
-      <a href="" className="home__social-icon" target="_blank">
-        <i className="uil uil-github"></i>
-      </a>
+      <a href="https://github.com/faid-terence" className="home__social-icon" target="_blank" rel="noreferrer">
+    <i className="uil uil-github"></i>
+</a>
+
     </div>
   );
 };
