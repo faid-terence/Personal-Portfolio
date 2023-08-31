007c00819879d1d7ed9472e86896b1c01655994d
Faid Terence JABO <faidterence@outlook.com>
2023-08-31 12:31:17 +0200

    feature(Portfolio):Medicare link updated

--- src/components/Portfolio/Data.js
+++ src/components/Portfolio/Data.js
@@ -4,58 +4,58 @@ import Work2 from "../../assets/my_e_ecomm.png";
 import Work4 from "../../assets/Doctor-book.png";
 import Work5 from "../../assets/codewithTerence.png";
 import Work6 from "../../assets/myweb.png";
-import Work7 from '../../assets/flix2.png'
-import Work8 from '../../assets/smoothies.png'
-export const projectsData = [ 
+import Work7 from "../../assets/flix2.png";
+import Work8 from "../../assets/smoothies.png";
+export const projectsData = [
   {
     id: 1,
-    image: Work8,
-    title: "Terence Smoothies(Auth)",
-    category: "design",
-    link:  'https://copper-armadillo-kilt.cyclic.app/'
-  },
-  {
-    id: 2,
-    image: Work2,
-    title: "Shop With Terence",
-    category: "app",
-    link: "https://faid-terence.github.io/My-Ecommerce-App-Frontend/"
-  },
-  {
-    id: 3,
     image: Work7,
     title: "Netflix Clone(Flix with Faid)",
     category: "web",
-    link: "https://stunna-flix-86l2.onrender.com/"
+    link: "https://flixwithfaid.netlify.app/",
   },
   {
-    id: 4,
+    id: 2,
     image: Work4,
     title: "Medicare Doctor Booking",
     category: "web",
-    link: "https://bookwithfaid.netlify.app/"
+    link: "https://bookwithfaid.netlify.app/",
+  },
+  {
+    id: 3,
+    image: Work1,
+    title: "E-commerce Web Design (UI/UX)",
+    category: "design",
+    link: "https://www.figma.com/file/uIqHkoLiY4wwAFojUDocwF/Ecommerce_predators?type=design&node-id=0-1&mode=design&t=irFwLCRcoqxehF5G-0  ",
+  },
+  {
+    id: 4,
+    image: Work6,
+    title: "Personal Portfolio Sample",
+    category: "web",
+    link: "https://faid-terence.github.io/Andela-Capstone-Project-Frontend/",
   },
   {
     id: 5,
     image: Work5,
     title: "Learn To Code With Faid",
     category: "Web",
-    link: "https://faid-terence.github.io/WEB-TECH-PROJECT-Y2/"
-    
+    link: "https://codewithfaid.netlify.app/",
   },
   {
     id: 6,
-    image: Work6,
-    title: "Personal Portfolio Sample",
-    category: "web",
-    link:"https://faid-terence.github.io/Andela-Capstone-Project-Frontend/"
+    image: Work2,
+    title: "Shop With Terence",
+    category: "app",
+    link: "https://faid-terence.github.io/My-Ecommerce-App-Frontend/",
   },
+
   {
     id: 7,
-    image: Work1,
-    title: "E-commerce Web Design (UI/UX)",
+    image: Work8,
+    title: "Terence Smoothies(Auth)",
     category: "design",
-    link : "https://www.figma.com/file/uIqHkoLiY4wwAFojUDocwF/Ecommerce_predators?type=design&node-id=0-1&mode=design&t=irFwLCRcoqxehF5G-0  "
+    link: "https://copper-armadillo-kilt.cyclic.app/",
   },
 ];
 
