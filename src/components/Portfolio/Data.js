3b72cc0b9cec982cb89498dc80829281fbc8bf2f
Faid Terence JABO <faidterence@outlook.com>
2023-09-11 21:06:45 +0200

    New Portfolio Added

--- src/components/Portfolio/Data.js
+++ src/components/Portfolio/Data.js
@@ -6,44 +6,52 @@ import Work5 from "../../assets/codewithTerence.png";
 import Work6 from "../../assets/myweb.png";
 import Work7 from "../../assets/flix2.png";
 import Work8 from "../../assets/smoothies.png";
+import Work9 from "../../assets/drive-clone.png"
 export const projectsData = [
   {
     id: 1,
+    image: Work9,
+    title: "Google Drive Clone",
+    category: "web",
+    link: "https://google-drive-clone-faid.vercel.app/",
+  },
+  {
+    id: 2,
     image: Work7,
     title: "Netflix Clone(Flix with Faid)",
     category: "web",
     link: "https://flixwithfaid.netlify.app/",
   },
   {
-    id: 2,
+    id: 3,
     image: Work4,
     title: "Medicare Doctor Booking",
     category: "web",
     link: "https://bookwithfaid.netlify.app/",
   },
   {
-    id: 3,
+    id: 4,
     image: Work1,
     title: "E-commerce Web Design (UI/UX)",
     category: "design",
     link: "https://www.figma.com/file/uIqHkoLiY4wwAFojUDocwF/Ecommerce_predators?type=design&node-id=0-1&mode=design&t=irFwLCRcoqxehF5G-0  ",
   },
   {
-    id: 4,
+    id: 5,
     image: Work6,
     title: "Personal Portfolio Sample",
     category: "web",
     link: "https://faid-terence.github.io/Andela-Capstone-Project-Frontend/",
   },
   {
-    id: 5,
+    id: 6,
     image: Work5,
     title: "Learn To Code With Faid",
     category: "Web",
     link: "https://codewithfaid.netlify.app/",
   },
   {
-    id: 6,
+    id: 7,
     image: Work2,
     title: "Shop With Terence",
     category: "app",
@@ -51,7 +59,7 @@ export const projectsData = [
   },
 
   {
-    id: 7,
+    id: 8,
     image: Work8,
     title: "Terence Smoothies(Auth)",
     category: "design",
